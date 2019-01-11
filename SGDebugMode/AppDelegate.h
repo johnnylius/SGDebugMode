8e124eb57eaa04cf92fc11536c77b3f1e11e2160
liuhuan <lh621@qq.com>
2019-01-12 01:20:47 +0800

    Initial Commit

---
+++ SGDebugMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGDebugMode
+//
+//  Created by Johnny on 2019/1/12.
+//  Copyright © 2019年 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGDebugMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGDebugMode
+//
+//  Created by Johnny on 2019/1/12.
+//  Copyright © 2019年 johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
